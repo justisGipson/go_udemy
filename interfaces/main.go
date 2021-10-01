40c732636b403d675cb1a938dc9f29ecb4ec35c9
Justis <justis710@gmail.com>
2021-10-01 15:02:07 -0400

    interfaces

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,1 +1,35 @@
+// interfaces
+// gonna make some chat bots
+
 package main
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+// ok to omit type value, and leave type
+// if value not being used
+func (eb englishBot) getGreeting() string {
+	return "hi there"
+}
+
+func (sb spanishBot) getGreeting() string {
+	return "Hola!"
+}
